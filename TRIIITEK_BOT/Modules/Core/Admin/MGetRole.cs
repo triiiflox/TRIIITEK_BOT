0cdd470d3daad6865bf1d905032a2daa25a55619
Wesley van Loon <KingTamotsu@outlook.com>
2020-02-18 19:38:07 +0100

    Added get all server roles

--- TRIIITEK_BOT/Modules/Core/Admin/MGetRole.cs
+++ TRIIITEK_BOT/Modules/Core/Admin/MGetRole.cs
@@ -1,9 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Threading.Tasks;
 
 namespace TRIIITekBot.Modules.Core.Admin {
-    public class MGetRole {
+    public class MGetRole : ModuleBase<SocketCommandContext> {
+        [Command("GetUserRoles")]
+        public Task GetUserRoles(SocketGuildUser user) {
+            List<SocketRole> roles = user.Roles.ToList();
+            var s = "";
+            foreach (var x in roles) {
+                s = s + "\n" + x;
+            }
+            return ReplyAsync(s);
+        }
 
+        [Command("GetServerRoles")]
+        public Task GetServerRoles()
+        {
+            List<IRole> roles = (Context.User as IGuildUser).Guild.Roles.ToList();
+            var s = "";
+            foreach (var x in roles)
+            {
+                s = s + "\n" + x;
+            }
+            return ReplyAsync(s);
+        }
     }
 }
